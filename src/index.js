b503758881d74e0853415e0f7b20ffe036b9ca0e
akash-0909 <akashdaravastuwar@gmail.com>
2026-08-10 22:15:59 +0530

    Utils discussion

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: "./.env"
 });
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("Error occurred while connecting to MongoDB", err);}
+); 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,18 @@
+class ApiError extends Error {
+    constructor(statusCode, 
+        message = "Internal Server Error",
+        errors = [],
+        statck=""
+        ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.message = message;
+        this.success = false;
+        if (statck) {
+            this.stack = statck;
+        }else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+    }
