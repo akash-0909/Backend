9cf639c2a908545a816dcc71acbfd1a3b1ff455c
akash-0909 <akashdaravastuwar@gmail.com>
2026-08-22 16:55:53 +0530

    .

--- src/db/index.js
+++ src/db/index.js
@@ -16,13 +16,24 @@ import { DB_NAME } from "../constants.js";
 //   }
 // })();
 const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`Connected to MongoDB database: ${connectionInstance.connection.host}`);
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-   process.exit(1); // Exit the process with an error code
-  }
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`,
+            {
+                family: 4
+            }
+        );
+
+        console.log(
+            `MongoDB connected: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log(
+            "Error connecting to MongoDB:",
+            error
+        );
+        process.exit(1);
+    }
 };
 
 export default connectDB;
